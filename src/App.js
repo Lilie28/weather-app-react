78a6116f30fa8573e485536105109ccb34e14524
Lilie28 <Lilie28@users.noreply.github.com>
2021-02-11 11:27:14 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import Weather from "./Weather";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+          Hello from React! 
+        </h1>
+        <Weather city="Paris"/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
